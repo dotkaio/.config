40f1da9a9437f7e33e40a0a11b56206802cc348e
dotkaio <github@kaio.email>
2024-12-16 02:30:19 -0800

    update

--- terminal/init.zsh
+++ terminal/init.zsh
@@ -2,18 +2,24 @@ export CONFIG="$HOME/.config"
 export TERMINAL="$CONFIG/terminal"
 export HISTFILE="$CONFIG/histfile"
 export HISTSIZE=10000
+#PATH="$PATH:$JAVA_HOME/bin"
+export CHROME_EXECUTABLE=/Applications/Chromium.app/Contents/MacOS/Chromium
+export HOMEBREW_NO_ENV_HINTS=           # Disable Homebrew's "please do not report this issue to Homebrew/cask/*" message
+export HOMEBREW_CASK_OPTS=--require-sha # Require SHA checksums for Casks
+export HOMEBREW_NO_ANALYTICS=           # Disable Homebrew's analytics
+export HOMEBREW_NO_AUTO_UPDATE=         # Disable Homebrew's automatic update
+export HOMEBREW_NO_INSECURE_REDIRECT=   # Disable Homebrew's insecure redirect warning
+export HOMEBREW_NO_INSTALL_CLEANUP=     # Disable Homebrew's cleanup of outdated versions
+export PATH=$GEM_HOME/bin:$PATH
+export PATH=$GEM_HOME/gems/bin:$PATH
 
-source $TERMINAL/alias.zsh
-source $TERMINAL/suggestion.zsh
-source $TERMINAL/setopt.zsh
-source $TERMINAL/functions.zsh
-source $TERMINAL/paths.zsh
-source $TERMINAL/export.zsh
-source $TERMINAL/bindkey.zsh
-source $TERMINAL/compdef.zsh
-source $TERMINAL/autoload.zsh
-source $TERMINAL/zstyle.zsh
-source $TERMINAL/highlight/init.zsh
+for file in $TERMINAL/*.zsh; do
+	source $file
+done
+
+rm $HOME/.zcompdump*
+autoload -Uz compinit
+compinit
 
 FPATH=$TERMINAL/completions:$FPATH
 
